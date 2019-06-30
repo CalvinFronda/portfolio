48e5b46729929b2ec14bd6bdecd54b9af1fa99be
Calvin Fronda <cfronda209@gmail.com>
2019-06-30 10:46:50 -0700

    added tool tip and disabled button

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
     // Add smooth scrolling to all links
+
+    $('.tooltipped').tooltip();
+
+
+
+
     $("a").on('click', function (event) {
 
         // Make sure this.hash has a value before overriding default behavior
@@ -22,5 +28,6 @@ $(document).ready(function () {
         } // End if
     });
 });
+
 
 
